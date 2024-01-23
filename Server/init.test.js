2631a85e93b63115e1108e83aa985b86d37ac69d
nishan <nishandudakia1@gmail.com>
2024-01-23 12:35:23 +0000

    create testShortlist.json containing the 40 selected countries. create test for if the function returns an array of arrays containing objects. create test for if the array is split into chunks of 4. create test for given an empty shortlist.

--- Server/init.test.js
+++ Server/init.test.js
@@ -1,6 +1,7 @@
 const each = require("jest-each").default;
 const {selectCountries, split} = require("./init.js")
 const testInputArray = require("./testInputArray.json")
+const testShortlist = require('./testShortlist.json')
 
 describe("selectCountries", () => {
 
@@ -26,7 +27,7 @@ describe("selectCountries", () => {
         expect(uniqueCountries.size).toBe(40);
       });
 
-    it('should skip any empty objects', () => {
+    it('should skip any objects that do not contain the info we need', () => {
         expect(selectCountries([],[{
             "flags": {
                 "png": "",
@@ -79,5 +80,21 @@ describe("split", () => {
         expect(split).toBeDefined();
         expect(split instanceof Function).toEqual(true);
     })
+
+    it('should return an array of arrays containing objects', () => {
+        expect(split(testShortlist)).toBeInstanceOf(Array);
+    
+        expect(split(testShortlist).every(chunk => Array.isArray(chunk))).toBe(true);
+    
+        expect(split(testShortlist).every(chunk => chunk.every(obj => typeof obj === 'object' && obj !== null))).toBe(true);
+      });
+
+    it('should split the array into chunks of 4', () => {
+        expect(split(testShortlist)).toHaveLength(10);
+      });
+
+    it('should handle an empty shortlist', () => {
+        expect(split([])).toHaveLength(0);
+      }); 
 
 })
