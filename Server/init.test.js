00c679cb6a0d95524e0ec0482ef469dddb661eb9
nishan <nishandudakia1@gmail.com>
2024-01-23 11:59:09 +0000

    create tests for checking if each object within the array contains only flag and name key value pairs. Furthermore, create test to check all selected countries are unique

--- Server/init.test.js
+++ Server/init.test.js
@@ -9,15 +9,20 @@ describe("selectCountries", () => {
         expect(selectCountries instanceof Function).toEqual(true);
     })
 
-    // it("Returns an empty object when passed an empty array", () => {
-    //     const result = selectCountries([{"flags":{"png":"","svg":"","alt":""},"name":{"common":"","official":"","nativeName":{"cat":{"official":"","common":""}}}}])
-    //     expect(result instanceof Object).toEqual(true);
-    //     expect(Object.keys(result).length).toBe(0);
-    // })
-
     it('should return an array with 40 objects', () => {    
-        // Assert
         expect(selectCountries([],testInputArray)).toHaveLength(40);
+      });
+
+    it('should only contain flag and name key value pairs', () => {    
+        expect(selectCountries([],testInputArray).every(obj => {
+            const keys = Object.keys(obj);
+            return keys.length === 2 && keys.includes('flag') && keys.includes('name');
+          })).toBe(true);;
+      });
+
+    it('should not add duplicate countries', () => {
+        const uniqueCountries = new Set(selectCountries([],testInputArray));
+        expect(uniqueCountries.size).toBe(40);
       });
 
 })
@@ -28,19 +33,5 @@ describe("split", () => {
         expect(split).toBeDefined();
         expect(split instanceof Function).toEqual(true);
     })
-
-    // it("Returns an empty object when passed an empty array", () => {
-    //     const result = countValues([])
-    //     expect(result instanceof Object).toEqual(true);
-    //     expect(Object.keys(result).length).toBe(0);
-    // })
-
-    // each([
-    //     [{ 1: 1, 2: 2, 3: 3 }, [1, 2, 2, 3, 3, 3]],
-    //     [{ "a": 2, "b": 1, "c": 1, "d": 2 }, ["a", "b", "c", "d", "d", "a"]],
-    //     [{ "a": 1, "A": 1, "cat": 1, 1: 2 }, ["a", "A", 1, 1, "cat"]],
-    // ]).test(`Returns %s when passed %s`, (expected, input) => {
-    //     expect(countValues(input)).toEqual(expected);
-    // })
 
 })
